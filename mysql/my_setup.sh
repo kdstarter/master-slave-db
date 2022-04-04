f57382ac37d3ab10f5d1085054c15190b035d7f9
Teddy Jiang <teddy.jiang@qq.com>
2022-04-04 12:53:03 +0800

    Changed db syncing mode as gtid_mode ON

--- mysql/my_setup.sh
+++ mysql/my_setup.sh
@@ -7,10 +7,11 @@ GRANT replication slave,replication client on *.* TO 'rep'@'%';
 mysql -h 127.0.0.1 -P 3306 -u rep -p'1234567'
 
 # step2: backup and restore initial db
-mysqldump -h 127.0.0.1 -P 3306 -u root -p'123' master_db --single-transaction --master-data=1 --flush-logs > tmp/mysql_init.sql #`date +%F_%H-%M-%S`-mysql-all.sql
-mysql -h 127.0.0.1 -P 3307 -u root -p'456' < tmp/mysql_init.sql
+# mysqldump -h 127.0.0.1 -P 3306 -u root -p'123' master_db --single-transaction --master-data=1 --flush-logs > tmp/mysql_init.sql #`date +%F_%H-%M-%S`-mysql-all.sql
+# mysql -h 127.0.0.1 -P 3307 -u root -p'456' < tmp/mysql_init.sql
+GRANT SElECT ON *.* TO 'viewer'@'%' IDENTIFIED BY "1234567";
 
 # setp3: on slave server, set master db
-change master to master_host='db1',master_user='rep',master_password='1234567',master_log_file='a37dc98c1c66-bin.000006',master_log_pos=154;
-start slave;
+change master to master_host='db1',master_user='rep',master_password='1234567',master_auto_position=1;
+start slave;              # stop slave;
 SHOW slave status\G;
